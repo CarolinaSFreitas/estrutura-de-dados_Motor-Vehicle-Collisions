3294d35670e77603052fc08fe19d3c15f5f6d40c
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-11-12 17:43:41 -0300

    fix on monday

--- colisoes.py
+++ colisoes.py
@@ -17,23 +17,76 @@ def titulo(texto, traco="-"):
 
 
 def total_acidentes():
-    titulo("Número Total de Acidentes")
-
+    titulo("Dados Gerais sobre Acidentes")
+    
     total = len(colisoes)
     print(f"O número total de acidentes é: {total}")
-    return total
+
+    mortes = sum(int(linha['NUMBER OF PERSONS KILLED']) if linha['NUMBER OF PERSONS KILLED'] else 0 for linha in colisoes)
+    print(f"Número de acidentes com mortes: {mortes}")
+
+    sem_mortes = total - mortes
+    print(f"Número de acidentes sem mortes: {sem_mortes}")
+
+    return total, mortes, sem_mortes
+
 
 def acidentes_bairros():
-    pass
+    titulo("Número de Acidentes por Bairro")
+
+    bairros = {}
+    for linha in colisoes:
+        bairro = linha['BOROUGH']
+        if bairro in bairros:
+            bairros[bairro] += 1
+        else:
+            bairros[bairro] = 1
+
+    for bairro, num_acidentes in bairros.items():
+        if bairro:
+            print(f"{bairro}: {num_acidentes} acidentes")
 
 
 def grafico_acidentes_ano():
-    pass
+    titulo("Gráfico de Acidentes por Bairro e Ano")
 
+    dados_grafico = {}              #cria dicio
+    for linha in colisoes:
+        bairro = linha['BOROUGH']
+        ano = linha['CRASH DATE'][-4:]
+
+        if bairro not in dados_grafico:
+            dados_grafico[bairro] = {}
+
+        if ano in dados_grafico[bairro]:
+            dados_grafico[bairro][ano] += 1
+        else:
+            dados_grafico[bairro][ano] = 1
+
+    for bairro, dados_ano in dados_grafico.items():
+        anos, num_acidentes = zip(*dados_ano.items())
+        plt.pie(num_acidentes, labels=[f'{ano} - {bairro}' for ano in anos], autopct='%1.1f%%', startangle=140)
+        plt.title(f'Acidentes por Bairro em {bairro}')
+        plt.show()
 
 def grafico_acidentes_c_mortes():
-    pass
+    titulo("Gráfico de Acidentes com Mortes por Bairro")
 
+    dados_grafico = {}          #cria dicio
+    for linha in colisoes:
+        bairro = linha['BOROUGH']
+        mortes = int(linha['NUMBER OF PERSONS KILLED'])
+
+        if mortes > 0:
+            if bairro in dados_grafico:
+                dados_grafico[bairro] += 1
+            else:
+                dados_grafico[bairro] = 1
+
+    bairros, num_mortes = zip(*dados_grafico.items())
+    plt.pie(num_mortes, labels=bairros, autopct='%1.1f%%', startangle=140)
+    plt.title('Acidentes com Mortes por Bairro')
+    plt.show()
 
 # ---------------------------------------------------------------------  Programa Principal
 carrega_dados()
